3347f8590c25417e5f17390243c402193ffa0be0
Shervinee <shervinee@gmail.com>
2017-09-18 16:20:58 -0400

    added the core.js which contains he modules controllers and all functions of angular

---
+++ public/core.js
@@ -0,0 +1,41 @@
+//this is the main angular file contaning a blue print all we need for our app to run (for now)
+
+var myTodo = angular.module('myTodo', []);
+
+function mainController($scope, $http){
+    //get request using our api which loads all the todos from database
+    $http.get('/api/todos')
+        .success(function(data){
+            //get all the todos so we can loop on them in frontend
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data){
+            console.log('Error: '+data);
+        });
+    
+    //handling adding a new todo, if successed we empty our form and console log the data 
+    $scope.createTodo = function(){
+        $http.post('api/todos', $scope.formData)
+        .success(function(data) {
+            $scope.formData = {};
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data){
+            console.log('Error: '+ data);
+        });
+    };
+
+    //handling deleting a tood when the user wants
+    $scope.deleteTodo = function(id){
+        $http.delete('/api/todos' +id)
+        .success(function(data){
+            $scope.todos = data
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' +data);
+        });
+    };
+}
